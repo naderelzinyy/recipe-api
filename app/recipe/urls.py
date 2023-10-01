e2da1563dd44c36a3ffbaa4eb3b250e2db734e94
Nader Elziny <naderelziny805@gmail.com>
2023-10-01 13:49:44 +0300

    added recipe urls.

---
+++ app/recipe/urls.py
@@ -0,0 +1,22 @@
+"""
+Recipe api urls.
+"""
+
+from django.urls import (
+    include,
+    path
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+router = DefaultRouter()
+router.register("recipes", views.RecipeViewSet)
+
+app_name = "recipe"
+
+urlpatterns = [
+    path("", include(router.urls))
+]
