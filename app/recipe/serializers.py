85537e14b15eddb3866a85ee81f0771f17b04a68
Nader Elziny <naderelziny805@gmail.com>
2023-10-01 13:47:54 +0300

    Created recipe serializer.

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""
+Recipe app serializer.
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for reipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ["id", "title", "time_minutes", "price", "link"]
+        read_only_fields = ["id"]
