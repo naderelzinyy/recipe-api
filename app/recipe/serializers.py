2c442d84c3b4aa28cb7ae11dbd3351de6f3c6dc0
Nader Elziny <naderelziny805@gmail.com>
2023-10-01 22:30:26 +0300

    Added a new serializer for recipe.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,9 +6,16 @@ from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for reipes."""
+    """Serializer for recipes."""
 
     class Meta:
         model = Recipe
         fields = ["id", "title", "time_minutes", "price", "link"]
         read_only_fields = ["id"]
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Recipe details serializer."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ["description"]
